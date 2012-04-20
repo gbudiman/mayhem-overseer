52018a9df7aa7862e9e425ad2d0e65334797f34a
wahyu.g <wahyu.g@0a832603-af20-e076-90a0-ed80a231868f>
2012-04-20 16:50:59 +0000

    Updated canada scratch dict

--- MayhemTownParser.py
+++ MayhemTownParser.py
@@ -2,11 +2,11 @@
 from xml.dom.minidom import parseString
 from xml.dom.minidom import Node
 
-f = open('scratchspace.txt')
+f = open('scratchspace_canada.txt')
 text = f.read()
 dom = parseString(text)
 xmlTag = dom.getElementsByTagName('option')
 for tag in xmlTag:
 	for data in tag.childNodes:
 		if data.nodeType == Node.TEXT_NODE:
-			print "".join(("\t", tag.getAttribute("value"), " => \"", data.data, "\","))
+			print "".join(("\t", tag.getAttribute("value"), " => \"", data.data.encode('ascii', 'replace'), "\","))
