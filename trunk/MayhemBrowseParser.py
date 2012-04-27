5681a56c2191083fb1ad840dcc0921ee7f5578c7
wahyu.g <wahyu.g@0a832603-af20-e076-90a0-ed80a231868f>
2012-04-27 05:42:53 +0000

    Fixed location search on casting parser. Continued on browse parser, data extraction regex completed.

--- trunk/MayhemBrowseParser.py
+++ trunk/MayhemBrowseParser.py
@@ -3,6 +3,7 @@ import re
 import sys
 import string
 from MayhemCastingParser import MayhemCastingParser
+from BrowseDataObject import BrowseDataObject
 
 class MayhemBrowseParser(MayhemCastingParser):
 	def __init__(self, verbosity):
@@ -13,14 +14,21 @@ class MayhemBrowseParser(MayhemCastingParser):
 		location = re.findall("([A-Z \'\-a-z]+)[,][ ]([A-Z \'\-a-z]+)[,][ ]([A-Z \'\-a-z]+)[\t]+(?:</td>)", result)
 		gender = re.findall("(?:data1\">)([FeMmalen/]+)", result)
 		lastActivity = re.findall("([A-Za-z.]+)[ ]([0-9]{1,2})[,][ ]([0-9]{4})", result)
-		#additionalLocation = re.findall
+		compensation = re.findall("(?:Compensation:</td>)[\n\r\t]+(?:<td>)([A-Z/ a-z]+)(?:</td>)", result)
+		experience = re.findall("(?:Experience:</td>)[\n\r\t]+(?:<td>)([A-Z/ a-z]+)(?:</td>)", result)
+		shootNudes = re.findall("(?:Shoot Nudes:</td>)[\n\r\t]+(?:<td>)(?:(yes|no))(?:</td>)", result)
 		
-		print len(profession)
-		print len(location)
-		print len(gender)
-		print len(lastActivity)
+		iProfession = iter(profession)
 		iLocation = iter(location)
 		iGender = iter(gender)
 		iLastActivity = iter(lastActivity)
-		for d in profession:
-			print d, iLocation.next(), iGender.next(), iLastActivity.next()
+		iShootNudes = iter(shootNudes)
+		iCompensation = iter(compensation)
+		iExperience = iter(experience)
+		browseCount = 0
+		
+		#for d in profession:
+		#	print d, iLocation.next(), iGender.next(), iLastActivity.next()
+		#	if d == 'Model':
+		#		print ' -- shoot nudes?', iShootNudes.next()
+		#	print ' --', iCompensation.next(), iExperience.next()
