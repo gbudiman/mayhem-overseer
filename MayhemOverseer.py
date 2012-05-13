bc59c7f7eeeff4a05302d769db78f7a79555dcb9
wahyu.g <wahyu.g@0a832603-af20-e076-90a0-ed80a231868f>
2012-05-13 14:05:34 +0000

    Updated skip casting/members indexing. Fixed crawler for members with no location.

--- MayhemOverseer.py
+++ MayhemOverseer.py
@@ -13,11 +13,18 @@ else:
 			print '-h \t\t You\'re getting it now'
 			print '-o:recollect \t Continue from pickled dictionary'
 			print '-o:testonly \t Run crawler without updating database'
+			print
+			print '-t:skipcasting \t Only index Members data'
+			print '-t:skipmembers \t Only index Casting data'
 			sys.exit(0)
 		if sys.argv[d] == '-o:recollect':
 			oList.append('recollect')
 		if sys.argv[d] == '-o:testonly':
 			oList.append('testonly')
+		if sys.argv[d] == '-t:skipcasting':
+			oList.append('skipcasting')
+		if sys.argv[d] == '-t:skipmembers':
+			oList.append('members')
 	
 	if 'recollect' in oList and 'testonly' in oList:
 		print '-o:recollect and -o:testonly cannot be active at the same time'
