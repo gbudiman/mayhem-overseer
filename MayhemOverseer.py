919dff2d38102c93c27339ae2c756296b3523a78
wahyu.g <wahyu.g@0a832603-af20-e076-90a0-ed80a231868f>
2012-12-08 05:20:05 +0000

    VIP confirmed.

--- MayhemOverseer.py
+++ MayhemOverseer.py
@@ -1,32 +1,14 @@
 # -*- coding: utf-8 -*-
-from MayhemMiner import MayhemMiner
+from Announcer import Announcer
+import signal
 import sys
 
 if len(sys.argv) == 1:
-	miner = MayhemMiner(1, list())
+	print 'Usage: python MayhemOverseer.py <your message here>'
 else:
-	oList = list()
-	for d in range(1, len(sys.argv)):
-		if sys.argv[d] == '-h':
-			print 'Usage: python MayhemOverseer.py -{x}:{argument} -{x}:{argument} ...'
-			print 'Where -{x}:{argument} is as follows:'
-			print '-h \t\t You\'re getting it now'
-			print '-o:recollect \t Continue from pickled dictionary'
-			print '-o:testonly \t Run crawler without updating database'
-			print
-			print '-t:skipcasting \t Only index Members data'
-			print '-t:skipmembers \t Only index Casting data'
-			sys.exit(0)
-		if sys.argv[d] == '-o:recollect':
-			oList.append('recollect')
-		if sys.argv[d] == '-o:testonly':
-			oList.append('testonly')
-		if sys.argv[d] == '-t:skipcasting':
-			oList.append('skipcasting')
-		if sys.argv[d] == '-t:skipmembers':
-			oList.append('members')
+	interval = 330
+	announcer = Announcer(interval)
+	message = ' '.join(sys.argv[e] for e in xrange(1, len(sys.argv)));
 	
-	if 'recollect' in oList and 'testonly' in oList:
-		print '-o:recollect and -o:testonly cannot be active at the same time'
-	else:
-		miner = MayhemMiner(1, oList)
+	announcer.prepare(message).send();
+	exit(0);
